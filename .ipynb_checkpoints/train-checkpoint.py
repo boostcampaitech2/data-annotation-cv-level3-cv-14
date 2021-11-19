1cc60f84c50e53a293f00339f783873561266588
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-11-19 02:46:44 +0000

    final save

--- .ipynb_checkpoints/train-checkpoint.py
+++ .ipynb_checkpoints/train-checkpoint.py
@@ -12,9 +12,9 @@ from torch.optim import lr_scheduler
 from tqdm import tqdm
 
 from east_dataset import EASTDataset
-from dataset import SceneTextDataset
+#from dataset import SceneTextDataset
+from dataset import SceneTextDataset_Ex as SceneTextDataset
 from model import EAST
-
 
 def parse_args():
     parser = ArgumentParser()
@@ -45,10 +45,14 @@ def parse_args():
 
 def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
                 learning_rate, max_epoch, save_interval):
-    dataset = SceneTextDataset(data_dir, split='train', image_size=image_size, crop_size=input_size)
+    dataset = SceneTextDataset(data_dir, split='split_train', image_size=image_size, crop_size=input_size)
     dataset = EASTDataset(dataset)
-    num_batches = math.ceil(len(dataset) / batch_size)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+
+    dataset_valid = SceneTextDataset(data_dir, split='split_valid', image_size=image_size, crop_size=input_size)
+    dataset_valid = EASTDataset(dataset_valid)
+    valid_loader = DataLoader(dataset_valid, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = EAST()
@@ -56,39 +60,63 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
 
-    model.train()
+    best_loss = 999
     for epoch in range(max_epoch):
-        epoch_loss, epoch_start = 0, time.time()
-        with tqdm(total=num_batches) as pbar:
-            for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
-                pbar.set_description('[Epoch {}]'.format(epoch + 1))
 
-                loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+        for stage in ['train', 'valid']:
+            if stage == 'train':
+                model.train()
+                loader = train_loader
+                color = 95
+            elif stage == 'valid':
+                model.eval()
+                loader = valid_loader
+                color = 96
+            epoch_loss, epoch_start = 0, time.time()
+            epoch_cls_loss, epoch_angle_loss, epoch_iou_loss = 0., 0., 0.
 
-                loss_val = loss.item()
-                epoch_loss += loss_val
+            for img, gt_score_map, gt_geo_map, roi_mask in tqdm(loader):
 
-                pbar.update(1)
-                val_dict = {
-                    'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                    'IoU loss': extra_info['iou_loss']
-                }
-                pbar.set_postfix(val_dict)
+                if stage == 'train':
+                    loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                elif stage == 'valid':
+                    with torch.no_grad():
+                        loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+
+                epoch_loss += loss.item()
+
+                epoch_cls_loss += extra_info['cls_loss']
+                epoch_angle_loss += extra_info['angle_loss']
+                epoch_iou_loss += extra_info['iou_loss']
+
+            epoch_loss /= len(loader)
+            epoch_cls_loss /= len(loader)
+            epoch_angle_loss /= len(loader)
+            epoch_iou_loss /= len(loader)
+            print(f'\n\033[{color}m {epoch}/{max_epoch} {stage}: '
+                  f'Mean loss: {epoch_loss:.4f}, '
+                  f'Cls loss: {epoch_cls_loss:.4f}, '
+                  f'Angle loss: {epoch_angle_loss:.4f}, '
+                  f'IoU loss: {epoch_iou_loss:.4f} | '
+                  f'Elapsed time: {timedelta(seconds=time.time() - epoch_start)}' + '\033[0m')
+
+            if stage == 'valid':
+                # Best score 모델 저장
+                if epoch_loss < best_loss:
+                    if not osp.exists(model_dir):
+                        os.makedirs(model_dir)
+                    ckpt_fpath = osp.join(model_dir, 'latest.pth')
+                    torch.save(model.state_dict(), ckpt_fpath)
+                    print(f"Best performance at epoch: {epoch}, Save model in {ckpt_fpath}")
+                    best_loss = epoch_loss
 
         scheduler.step()
 
-        print('Mean loss: {:.4f} | Elapsed time: {}'.format(
-            epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
 
-        if (epoch + 1) % save_interval == 0:
-            if not osp.exists(model_dir):
-                os.makedirs(model_dir)
-
-            ckpt_fpath = osp.join(model_dir, 'latest.pth')
-            torch.save(model.state_dict(), ckpt_fpath)
 
 
 def main(args):
---
+++ dataset_split.py
@@ -0,0 +1,46 @@
+import json
+import random
+random.seed(7)
+
+sourceFile1 = '../input/data/ICDAR17_Korean/ufo/train.json'
+sourceFile2 = '../input/data/Camper_Data/ufo/annotation.json'
+sourceFile3 = '../input/data/ReposData/ufo/ufoannotation.json'
+
+targetDir = '../input/data/'
+
+with open(sourceFile1,'r') as f:
+    jsonData_All = json.load(f)
+with open(sourceFile2,'r') as f:
+    jsonData2 = json.load(f)
+with open(sourceFile3,'r') as f:
+    jsonData3 = json.load(f)
+
+print(len(jsonData_All['images']))
+jsonData_All['images'].update(jsonData2['images'])
+print(len(jsonData_All['images']))
+jsonData_All['images'].update(jsonData3['images'])
+print(len(jsonData_All['images']))
+#jsonData_All = jsonData3
+
+imageNames = list(jsonData_All["images"].keys())
+random.shuffle(imageNames)
+
+pivot = int(len(imageNames) * 0.8)
+trainNames = imageNames[:pivot]
+validNames = imageNames[pivot:]
+
+jsonData_train = {"images":{}}
+jsonData_valid = {"images":{}}
+
+for jsonData, names in zip([jsonData_train, jsonData_valid], [trainNames, validNames]):
+    for name in names:
+        jsonData["images"][name] = jsonData_All["images"][name]
+
+print(len(jsonData_train))
+print(len(jsonData_valid))
+
+with open(targetDir+"split_train.json", 'w') as f:
+    json.dump(jsonData_train, f, indent="\t")
+
+with open(targetDir+"split_valid.json", 'w') as f:
+    json.dump(jsonData_valid, f, indent="\t")
